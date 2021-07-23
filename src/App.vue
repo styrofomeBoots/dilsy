57ea5885bc8cab6b8e764955ca0190a84664a03e
Seth <seth.newbill@temperpack.com>
2021-07-23 15:26:17 -0400

    added marker boundaries and programmatically find the center

--- src/App.vue
+++ src/App.vue
@@ -20,3 +20,9 @@
     }),
   };
 </script>
+
+<style>
+  html {
+    overflow-y: hidden;
+  }
+</style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,14 +1,7 @@
 <template>
   <div style="height: 100vh;">
-    <l-map
-      style="height: 100%; width: 100%"
-      :zoom="zoom"
-      :center="center"
-      @update:zoom="zoomUpdated"
-      @update:center="centerUpdated"
-      @update:bounds="boundsUpdated"
-    >
-      <l-tile-layer :url="url"></l-tile-layer>
+    <l-map style="height: 100%; width: 100%" :zoom="zoom" :center="center">
+      <l-tile-layer :url="mapUrl"></l-tile-layer>
       <l-circle-marker
         v-for="marker in markers"
         :key="marker.id"
@@ -35,17 +28,22 @@
     },
     data() {
       return {
-        url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-        zoom: 13.1,
-        center: [38.89, -77.03],
-        bounds: null,
         stationInformation:
           'https://gbfs.capitalbikeshare.com/gbfs/en/station_information.json',
         stationStatus:
           'https://gbfs.capitalbikeshare.com/gbfs/en/station_status.json',
+        mapUrl: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+        zoom: 13.1,
+        center: [],
         markers: [],
-        radius: 3,
+        radius: 2,
         color: 'gray',
+        latMax: 38.943837,
+        latMin: 38.843422,
+        lonMax: -76.925315,
+        lonMin: -77.179832,
+        columns: 7, // columns to represent C,D,E,F,G,A,B
+        rows: 7, // rows to represent octaves
       };
     },
     methods: {
@@ -60,9 +58,21 @@
       },
     },
     created() {
+      // find center of map
+      let centerLat = (this.latMin + this.latMax) / 2;
+      let centerLon = (this.lonMin + this.lonMax) / 2;
+      this.center = [centerLat, centerLon];
+      // get station locations
       axios.get(this.stationInformation).then((data) => {
         data.data.data.stations.forEach((el) => {
-          this.markers.push({ id: el.station_id, latLong: [el.lat, el.lon] });
+          if (
+            el.lat >= this.latMin &&
+            el.lat <= this.latMax &&
+            el.lon >= this.lonMin &&
+            el.lon <= this.lonMax
+          ) {
+            this.markers.push({ id: el.station_id, latLong: [el.lat, el.lon] });
+          }
         });
       });
     },
